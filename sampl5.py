b2e1bc2b07ecbf8e0275d7094444ac4081a563fa
chandrakala199 <47442191+chandrakala199@users.noreply.github.com>
2019-02-08 15:05:03 +0530

    Create sampl5.py

---
+++ sampl5.py
@@ -0,0 +1,9 @@
+inputValue=raw_input()
+a,b,c=inputValue.split(" ")
+a,b,c=int(a),int(b),int(c)
+if((a>b) and (a>c)):
+    print(a)
+elif((b>c) and (b>a)):
+    print(b)
+elif((c>a) and (c>b)):
+    print(c)
